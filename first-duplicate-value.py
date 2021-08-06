a4a950649801b4fe8c7e63a6a572d37b33577a9b
shenoyrohit <shenoyrohit1992@gmail.com>
2021-08-05 19:16:26 -0700

    Update first-duplicate-value.py

--- first-duplicate-value.py
+++ first-duplicate-value.py
@@ -8,3 +8,12 @@ def firstDuplicateValue(array):
         else:
             myDict[elem] = True
     return -1
+
+# Time: O(N) | Space: O(1)
+def firstDuplicateValueOptimized(array):
+	for elem in array:
+		if array[abs(elem) - 1] < 0:
+			return abs(elem)
+		else:
+			array[abs(elem) - 1] = array[abs(elem) - 1] * -1
+	return -1
